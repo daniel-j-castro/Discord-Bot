4aab8b3530054117721788f006f780509482b1ab
unknown <djc.usa98@gmail.com>
2022-06-15 23:58:58 -0500

    test

---
+++ index.ts
@@ -0,0 +1,26 @@
+import DiscordJS, { Intents } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS
+    ]
+});
+
+client.on('ready', () => {
+    console.log('The bot is on!');
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping'){
+        message.reply({
+            content: "Pong!",
+        });
+    }
+});
+
+client.login(process.env.TOKEN);
